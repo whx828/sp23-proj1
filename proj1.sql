8ea60c23eb8f7b8f5310ccca0d3875efd035ff26
whx828 <whx19990828@gmail.com>
2023-05-31 20:45:07 +0800

    proj1 done.

--- proj1.sql
+++ proj1.sql
@@ -15,98 +15,223 @@ DROP VIEW IF EXISTS q4ii;
 DROP VIEW IF EXISTS q4iii;
 DROP VIEW IF EXISTS q4iv;
 DROP VIEW IF EXISTS q4v;
+DROP VIEW IF EXISTS CAcollege;
+DROP VIEW IF EXISTS slg;
+DROP VIEW IF EXISTS lslg;
+DROP VIEW IF EXISTS salary_statistics;
+DROP VIEW IF EXISTS maxid;
+DROP VIEW IF EXISTS bins_statistics;
+DROP VIEW IF EXISTS bins;
 
 -- Question 0
 CREATE VIEW q0(era)
 AS
-  SELECT 1 -- replace this line
+  SELECT MAX(era)
+  FROM pitching
 ;
 
 -- Question 1i
 CREATE VIEW q1i(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE weight > 300;
 ;
 
 -- Question 1ii
 CREATE VIEW q1ii(namefirst, namelast, birthyear)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, birthyear
+  FROM people
+  WHERE namefirst LIKE '% %'
+  ORDER BY namefirst, namelast;
 ;
 
 -- Question 1iii
 CREATE VIEW q1iii(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  ORDER BY birthyear;
 ;
 
 -- Question 1iv
 CREATE VIEW q1iv(birthyear, avgheight, count)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT birthyear, AVG(height), COUNT(*)
+  FROM people
+  GROUP BY birthyear
+  HAVING AVG(height) > 70
+  ORDER BY birthyear;
 ;
 
 -- Question 2i
 CREATE VIEW q2i(namefirst, namelast, playerid, yearid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, playerid, yearid
+  FROM people NATURAL JOIN halloffame 
+  WHERE halloffame.inducted = 'Y'
+  ORDER BY yearid DESC, playerid
 ;
 
 -- Question 2ii
+CREATE VIEW CAcollege(playerid, schoolid)
+AS 
+  SELECT c.playerid, c.schoolid 
+  FROM collegeplaying c INNER JOIN schools s
+  ON c.schoolid = s.schoolid
+  WHERE s.schoolState = 'CA';
+
 CREATE VIEW q2ii(namefirst, namelast, playerid, schoolid, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT namefirst, namelast, q.playerid, schoolid, yearid
+  FROM q2i q INNER JOIN CAcollege c
+  ON q.playerid = c.playerid
+  ORDER BY yearid DESC, schoolid, q.playerid;
 ;
 
 -- Question 2iii
 CREATE VIEW q2iii(playerid, namefirst, namelast, schoolid)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT q.playerid, namefirst, namelast, schoolid
+  FROM q2i q LEFT OUTER JOIN collegeplaying c
+  ON q.playerid = c.playerid
+  ORDER BY q.playerid DESC, schoolid;
 ;
 
 -- Question 3i
+CREATE VIEW slg(playerid, yearid, AB, slgval)
+AS 
+  SELECT playerid, yearid, AB, (H + H2B + 2*H3B + 3*HR + 0.0)/(AB + 0.0)
+  FROM batting
+;
+
 CREATE VIEW q3i(playerid, namefirst, namelast, yearid, slg)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, s.yearid, s.slgval
+  FROM people p INNER JOIN slg s
+  ON p.playerid = s.playerid
+  WHERE s.AB > 50
+  ORDER BY s.slgval DESC, s.yearid, p.playerid
+  LIMIT 10
 ;
 
 -- Question 3ii
+CREATE VIEW lslg(playerid, lslgval)
+AS 
+  SELECT playerid, (SUM(H) + SUM(H2B) + 2 * SUM(H3B) + 3 * SUM(HR) + 0.0)/(SUM(AB) + 0.0)
+  FROM batting
+  GROUP BY playerid
+  HAVING SUM(AB) > 50
+;
+
 CREATE VIEW q3ii(playerid, namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT p.playerid, p.namefirst, p.namelast, l.lslgval
+  FROM people p INNER JOIN lslg l
+  ON p.playerid = l.playerid
+  ORDER BY l.lslgval DESC, p.playerid
+  LIMIT 10
 ;
 
 -- Question 3iii
 CREATE VIEW q3iii(namefirst, namelast, lslg)
 AS
-  SELECT 1, 1, 1 -- replace this line
+  SELECT p.namefirst, p.namelast, l.lslgval
+  FROM people p INNER JOIN lslg l
+  ON p.playerid = l.playerid
+  WHERE l.lslgval >
+    (
+      SELECT lslgval 
+      FROM lslg 
+      WHERE playerid = 'mayswi01'
+    )
 ;
 
 -- Question 4i
 CREATE VIEW q4i(yearid, min, max, avg)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT yearid, MIN(salary), MAX(salary), AVG(salary)
+  FROM salaries
+  GROUP BY yearid
+  ORDER BY yearid
 ;
 
+-- Helper table for 4ii
+DROP TABLE IF EXISTS binids; -- 如果存在 binids 表则删除它
+CREATE TABLE binids(binid); -- 创建 binids 表，只有一个列 binid
+INSERT INTO binids VALUES (0), (1), (2), (3), (4), (5), (6), (7), (8), (9); -- 插入 10 个 binid，从 0 到 9， 10 个 binid
+
 -- Question 4ii
+CREATE VIEW bins_statistics(binstart, binend, width) -- 创建 bins_statistics 视图，包含 3 个列
+AS 
+  SELECT MIN(salary), MAX(salary), CAST (((MAX(salary) - MIN(salary))/10) AS INT) -- 计算 salary 的最小值，最大值，以及每个 bin 的宽度
+  FROM salaries
+;
+
+CREATE VIEW bins(binid, binstart, width) -- 创建 bins 视图，包含 3 个列
+AS 
+  SELECT CAST ((salary/width) AS INT), binstart, width -- 计算每个 bin 的 binid。
+  FROM salaries, bins_statistics -- 从 salaries 表和 bins_statistics 表中获取数据
+  WHERE yearid = 2016 -- 选择 2016 年的 salary，binstart，以及 width 进行计算
+;
+
 CREATE VIEW q4ii(binid, low, high, count)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT binid, 507500.0+binid*3249250,3756750.0+binid*3249250, count(*)
+  from binids, salaries
+  where (salary between 507500.0+binid*3249250 and 3756750.0+binid*3249250 )and yearID='2016'
+  group by binid
 ;
 
 -- Question 4iii
+CREATE VIEW salary_statistics(yearid, minsa, maxsa, avgsa)
+AS 
+  SELECT yearid, MIN(salary), MAX(salary), AVG(salary)
+  FROM salaries
+  GROUP BY yearid
+;
+
 CREATE VIEW q4iii(yearid, mindiff, maxdiff, avgdiff)
 AS
-  SELECT 1, 1, 1, 1 -- replace this line
+  SELECT s1.yearid, s1.minsa - s2.minsa, s1.maxsa - s2.maxsa, s1.avgsa - s2.avgsa
+  FROM salary_statistics s1
+  INNER JOIN salary_statistics s2
+  ON s1.yearid - 1 = s2.yearid
+  ORDER BY s1.yearid
 ;
 
 -- Question 4iv
-CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
+CREATE VIEW maxid(playerid, salary, yearid)
 AS
-  SELECT 1, 1, 1, 1, 1 -- replace this line
-;
--- Question 4v
-CREATE VIEW q4v(team, diffAvg) AS
-  SELECT 1, 1 -- replace this line
+  SELECT playerid, salary, yearid
+    FROM salaries 
+    WHERE (yearid = 2000 AND salary = 
+          (SELECT MAX(salary)
+          FROM salaries s1
+          WHERE s1.yearid = 2000)
+          )
+          OR 
+          (yearid = 2001 AND salary =
+          (SELECT MAX(salary)
+          FROM salaries s2
+          WHERE s2.yearid = 2001)
+          )
 ;
 
+CREATE VIEW q4iv(playerid, namefirst, namelast, salary, yearid)
+AS
+  SELECT p.playerid, p.namefirst, p.namelast, m.salary, m.yearid
+  FROM people p INNER JOIN maxid m
+  ON p.playerid = m.playerid
+;
+
+-- Question 4v
+CREATE VIEW q4v(team, diffAvg) AS
+  SELECT a.teamid, MAX(s.salary) - MIN(s.salary)
+  FROM allstarfull a INNER JOIN salaries s 
+  ON a.playerid = s.playerid AND a.yearid = s.yearid
+  WHERE s.yearid = 2016
+  GROUP BY a.teamid
+;
